1b19ac1d4927d4e0d26368fef4131db8d99898ac
user <samsonovarteem@gmail.com>
2024-03-26 23:29:06 +0300

    dz2

--- DZ/dz2.py
+++ DZ/dz2.py
@@ -1,16 +1,4 @@
-def outer(a,b,c):
-    s=0
-
-    def inner(i,j):
-        nonlocal s
-        s += i *j
-
-    inner(a,b)
-    inner(a,c)
-    inner(b,c)
-    return 2*s
-
-
-print(outer(2,4,6))
-print(outer(5,8,2))
-print(outer(1,6,8))
+str1 = "Я изучаю Nuthon. мне нравится Nuthon. Nuthon очень интересный язык програмирования."
+print(str1)
+str2 =str1.replace("Nuthon","Puthon")
+print(str2)
