99bd0359e68b28bc99af874a73a10f2d56445248
user <samsonovarteem@gmail.com>
2024-04-01 20:23:17 +0300

    dz1

--- DZ/dz1.py
+++ DZ/dz1.py
@@ -1,13 +1,10 @@
-def avg(fn):
-    def wrap(*args):
-        return fn(*args) / len(args)
+text = ("Ежевику для ежат Принесли два ежат. Принесли два ежа. Еживику еле-еле Ежата возле ели съели.")
+text = text.split(" ")
+i = 0
+for word in text:
 
-    return wrap
+    if word[0].lower() == "е":
 
+        i = i+1;
 
-@avg
-def summa(*args):
-    return sum(args)
-
-
-print(summa(2,3,3,4))
+print (i)
