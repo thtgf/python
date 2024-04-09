f1f8120dc5c8fd78502b3cfd0425d7fb0f7b9488
user <samsonovarteem@gmail.com>
2024-04-09 22:37:14 +0300

    dz1

--- DZ/dz1.py
+++ DZ/dz1.py
@@ -1,10 +1,22 @@
-text = ("Ежевику для ежат Принесли два ежат. Принесли два ежа. Еживику еле-еле Ежата возле ели съели.")
-text = text.split(" ")
-i = 0
-for word in text:
+file = ("text2.txt")
 
-    if word[0].lower() == "е":
+f = open(file, "w")
+f.write("замена строки в текстовом файле;\nизменить строку в списке;\nзаписать список в файл;\n")
+f.close()
 
-        i = i+1;
+f = open(file,'r')
+read_line = f.readlines()
+f.close()
 
-print (i)
+print(read_line)
+pos1 = int(input("pos1 ="))
+pos2 = int(input("pos2 ="))
+if 0 <= pos1 < len(read_line) and 0<= pos2 < len(read_line):
+    read_line[pos1],read_line[pos2] = read_line[pos2],read_line[pos1]
+else:
+    print("Такой строки нет")
+print(read_line)
+
+f = open(file,"w")
+f.writelines(read_line)
+f.close()
