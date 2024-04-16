a4447640f725be081f14430172a2ee772ffb6829
user <samsonovarteem@gmail.com>
2024-04-16 23:46:47 +0300

    dz

--- DZ/dz1.py
+++ DZ/dz1.py
@@ -1,22 +1,14 @@
-file = ("text2.txt")
+import os
 
-f = open(file, "w")
-f.write("замена строки в текстовом файле;\nизменить строку в списке;\nзаписать список в файл;\n")
-f.close()
+dir_name = "nested1"
 
-f = open(file,'r')
-read_line = f.readlines()
-f.close()
+objs = os.listdir(dir_name)
+print(objs)
 
-print(read_line)
-pos1 = int(input("pos1 ="))
-pos2 = int(input("pos2 ="))
-if 0 <= pos1 < len(read_line) and 0<= pos2 < len(read_line):
-    read_line[pos1],read_line[pos2] = read_line[pos2],read_line[pos1]
-else:
-    print("Такой строки нет")
-print(read_line)
-
-f = open(file,"w")
-f.writelines(read_line)
-f.close()
+for obj in objs:
+    p = os.path.join(dir_name,obj)
+    #print(p)
+    if os.path.isfile(p):
+        print(f"{obj} - file - {os.path.getsize(p)}bytes")
+    elif os.path.isdir(p):
+        print(f"{obj} - dir")
