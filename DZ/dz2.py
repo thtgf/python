d69e785b930a9ee89b069a375b23ba52142b8272
user <samsonovarteem@gmail.com>
2024-04-24 22:10:53 +0300

    dz2

--- DZ/dz2.py
+++ DZ/dz2.py
@@ -1,2 +1,30 @@
-n = [-2,3,8,-11,-4,6]
-numbers = sum(1 for x in n if x <
+import math
+
+
+class Area:
+    count =0
+    @staticmethod
+    def triangle_area_1(a, b ,c):
+        p = (a+b+c) /2
+        Area.count +=1
+        return math.sqrt(p*(p-a)*(p-b)*(p-c))
+    @staticmethod
+    def triangle_area_2(a,h):
+        Area.count += 1
+        return 0.5*a*h
+    @staticmethod
+    def square_area(a):
+        Area.count += 1
+        return a**2
+    @staticmethod
+    def rect_area(a,b):
+        Area.count += 1
+        return a*b
+    @staticmethod
+    def get_count():
+        return Area.count
+print(f"Площадь треугольника по формуле Герона:{Area.triangle_area_1(3,4,5)}")
+print(f"Площадь треугольнига через основание и высоту:{Area.triangle_area_2(6,7)}")
+print(f"Площадь квадрата:{Area.square_area(7)}")
+print(f"Площадь прямоугольника:{Area.rect_area(2,6)}")
+print(f"Количество подсчётов площади:{Area.get_count()}")
