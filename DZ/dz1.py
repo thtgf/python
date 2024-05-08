e76804aece02a4115c7e7fa2779efd8d70f8c69a
user <samsonovarteem@gmail.com>
2024-05-08 23:55:22 +0300

    DZ

--- DZ/dz1.py
+++ DZ/dz1.py
@@ -1,23 +1,29 @@
-class Student:
-    def __init__(self,name):
-        self.name = name
-        self.note =self.Notebook()
-    def show(self):
-        print(self.name,end="")
-        self.note.show()
+class Clock:
+    __DAY = 86400  # Число секунд в дне
 
-    class Notebook:
-        def __init__(self):
-            self.brand ="HP"
-            self.cpu ="i7"
-            self.ram =16
+    def __init__(self, sec: int):
+        if not isinstance(sec, int):
+            raise ValueError("Секунды должны быть целым числом")
+        self.sec = sec % self.__DAY
 
-        def show(self):
-            print(f"=>{self.brand},{self.cpu},{self.ram}")
+        def get_format_time(self):
+            s = self.sec % 60
+            m = (self.sec // 60) % 60
+            h = (self.sec // 3600) % 24
+            return f"{Clock.__get_form(h)}:{Clock.__get_form(m)}:{Clock.__get_form(s)}"
 
-
-s1 = Student("Roman")
-s2 = Student("Vladimir")
-
-s1.show()
-s2.show()
+        @staticmethod
+        def __get_form(x):
+            return str(x) if x > 9 else "0" + str(x)
+        def __getitem__(self, item):
+            if not isinstance(item, str):
+                raise ValueError("Ключ должен быть строкой")
+            if item == "hour":
+                return self.sec // 3600 % 24
+            if item == "min":
+                return self.sec // 60 % 60
+            if item == "sec":
+                return self.sec % 60
+            c1 = Clock(80000)
+            print(c1.get_format_time())
+            print(c1["hour"], c1["min"], c1["sec"])
