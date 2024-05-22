c4ac394c4c5e04deca87d6ea8ade88201cb73936
user <samsonovarteem@gmail.com>
2024-05-23 00:54:01 +0300

    dz1

--- DZ/dz1.py
+++ DZ/dz1.py
@@ -1,8 +1,32 @@
-class Order:
-    def  __init__(self,name,price,volume):
-        self.name= name
-        self.price= price
-        self.volume= volume
+import json
+from random import choice
+def gen_person():
+    name =''
+    tel =''
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    while len(name) != 7:
+        name += choice(letters)
 
-o=Order("apple",5,10)
-print((o))
+    while len(tel) != 10:
+        tel += choice(nums)
+
+    person = {
+        'name': name,
+        "tel": tel
+    }
+    return person,tel
+
+def write_json(person_dict,num):
+    try:
+        data = json.load(open('persons1.json'))
+    except FileNotFoundError:
+        data = {}
+
+    data[num]=person_dict
+    with open('persons1.json', 'w') as f:
+        json.dump(data, f, indent=2)
+for i in range(5):
+    write_json(gen_person()[0],gen_person()[1])
+
+
