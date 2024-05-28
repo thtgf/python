278982832741106f2823b367d3d683f4cc058421
user <samsonovarteem@gmail.com>
2024-05-29 01:43:41 +0300

    dz2

--- DZ/dz2.py
+++ DZ/dz2.py
@@ -1,30 +1,91 @@
-import math
+import json
+data = {}
+class CountryCapital:
+    def __init__(self, country, capital):
+        self.country = country
+        self.capital = capital
+        data[self.country] = self.capital
 
+        def __str__(self):
+            return f"{self.country}: {self.capital}"
 
-class Area:
-    count =0
-    @staticmethod
-    def triangle_area_1(a, b ,c):
-        p = (a+b+c) /2
-        Area.count +=1
-        return math.sqrt(p*(p-a)*(p-b)*(p-c))
-    @staticmethod
-    def triangle_area_2(a,h):
-        Area.count += 1
-        return 0.5*a*h
-    @staticmethod
-    def square_area(a):
-        Area.count += 1
-        return a**2
-    @staticmethod
-    def rect_area(a,b):
-        Area.count += 1
-        return a*b
-    @staticmethod
-    def get_count():
-        return Area.count
-print(f"Площадь треугольника по формуле Герона:{Area.triangle_area_1(3,4,5)}")
-print(f"Площадь треугольнига через основание и высоту:{Area.triangle_area_2(6,7)}")
-print(f"Площадь квадрата:{Area.square_area(7)}")
-print(f"Площадь прямоугольника:{Area.rect_area(2,6)}")
-print(f"Количество подсчётов площади:{Area.get_count()}")
+        @staticmethod
+        def load_data(filename):
+            try:
+                date = json.load(open(filename))
+            except FileNotFoundError:
+                date = {}
+            finally:
+                return date
+
+        @staticmethod
+        def add_country(filename):
+            country_name = input("Введите название страны: ")
+            capital_name = input("Введите название столицы: ")
+            # try:
+            #     date = json.load(open(filename))
+            # except FileNotFoundError:
+            #     date = {}
+            date = CountryCapital.load_data(filename)
+
+            date[country_name] = capital_name
+            with open(filename, "w") as f:
+                json.dump(date, f, indent=2)
+
+        @staticmethod
+        def load_from_file(filename):
+            with open(filename, "r") as f:
+                print(json.load(f))
+
+        @staticmethod
+        def delete_country(filename):
+            del_country = input("Введите название страны: ")
+
+            date = CountryCapital.load_data(filename)
+            if del_country in date:
+                del date[del_country]
+                with open(filename, "w") as f:
+                    json.dump(date, f, indent=2)
+            else:
+                print("Такой страны в базе нет")
+
+        @staticmethod
+        def search_data(filename):
+            country = input("Введите название страны: ")
+            date = CountryCapital.load_data(filename)
+            if country in date:
+                print(f"Страна {country} столица {date[country]} есть в словаре")
+            else:
+                print(f"Страны {country} нет в словаре")
+
+        @staticmethod
+        def edit_data(filename):
+            country = input("Введите страну для корректировки: ")
+            new_capital = input("Введите новое название столицы: ")
+            date = CountryCapital.load_data(filename)
+            if country in date:
+                date[country] = new_capital
+                with open(filename, "w") as f:
+                    json.dump(date, f, indent=2)
+            else:
+                    print("Такой страны в базе нет")
+file = 'list_capital.json'
+index = ''
+while True:
+    index = input("Выбор действия:\n1 - добавление данных\n2 - удаление данных\n3 - поиск данных\n4 -"
+                      "редактирование данных\n5 - просмотр данных\n6 - завершение работы\nВвод: ")
+    if index == "1":
+        CountryCapital.add_country(file)
+    elif index == "2":
+        CountryCapital.delete_country(file)
+    elif index == "3":
+        CountryCapital.search_data(file)
+    elif index == "4":
+        CountryCapital.edit_data(file)
+    elif index == "5":
+        CountryCapital.load_from_file(file)
+    elif index == "6":
+        break
+    else:
+        print("Введен некорректный номер")
+
